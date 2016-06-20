62223fb93dd98fcfedc5413ede3e3c3748f67736
PowChow <pauline.chow@gmail.com>
2016-06-19 18:53:39 -0700

    added week 3 and week 4 materials

--- exercises/week_02 code_challenge_numoccurences.py
+++ exercises/week_02 code_challenge_numoccurences.py
@@ -2,14 +2,19 @@
 Wednesday Morning Challenge
 
 Question: 
-The numberOfOccurrences function must return the number of occurrences of an element in an array.
+The numberOfOccurrences function must return the 
+number of 
+occurrences of an element in an array.
 
 Example: 
 sample = [0,1,2,2,3]
-number_of_occurrences(0) == 1
-number_of_occurrences(4) == 0
-number_of_occurrences(2) == 2
-number_of_occurrences(3) == 1
+xs = 0
+
+assert 1 == 1
+assert number_of_occurrences([0,1,2,2,3], 0) == 1
+assert number_of_occurrences([0,1,2,2,3], 4) == 0
+assert number_of_occurrences([0,1,2,2,3], 2) == 2
+assert number_of_occurrences([0,1,2,2,3], 3) == 1
 
 Starter code:
 
@@ -17,6 +22,17 @@ def number_of_occurrences(sample, xs)
 	pass 
 
 '''
+
+
+
+
+
+
+
+
+
+
+
 #Method 1
 def number_of_occurrences(s, xs):
     total = 0
@@ -25,28 +41,70 @@ def number_of_occurrences(s, xs):
             total+=1
     return total
 
+
+
+
+
+
+
 #Method 2
 def number_of_occurrences(s, xs):
-    return xs.count(s)
+    return s.count(xs)
+
+
+
+
+
+
+
+
 
 #Method 3
 
 def number_of_occurrences(s, xs):
-    return sum(s == y for y in xs)
+    return sum(xs == y for y in s)
+
+
+
+
+
+
+
+
+
+
+
 
 #Method 4 - helper package collections
 
-from collections import Counter
 def number_of_occurrences(s, xs):
-    return Counter(xs)[s]
+    from collections import Counter
+
+    print Counter(sample)[xs]
 
 #Method 5 - lambda functions
-def number_of_occurrences(s, xs):
-    equalsS = lambda x: x == s  
-    return len(filter(equalsS ,xs))
+def number_of_occurrences(sample, xs):
+    equalsS = lambda x: x == xs  
+    return len(filter(equalsS ,sample))
+
+# This is equivalent 
+def number_of_occurrences(sample, xs): 
+    tmp = [item for item in sample if item == xs] 
+    return len(tmp)
 
 # If time introduction assertions
 # conda install or pip install pytest
 import pytest 
 
 assert 
+
+
+
+
+
+
+
+
+
+
+
